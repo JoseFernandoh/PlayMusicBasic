0159939639c194b3061b21dad2d1d33609b65b76
JoseFernandoh <josecernandl1565@gmail.com>
2021-12-16 13:30:41 -0300

    Version 1.5.0

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module PlayMusicBasic {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+    requires javafx.graphics;
+
+    opens br.com.PlayMusicBasic.tela;
+    opens br.com.PlayMusicBasic;
+}
